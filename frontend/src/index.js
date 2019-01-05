1e2d4bde4bca485c7ef43f59504d4d2ed26c9c9c
Peter Zeng <pzeng.pzeng@gmail.com>
2019-01-05 15:45:16 -0800

    Complete dictionary parsing for difficulty levels

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // We will create this component shortly
-import Root from './components/root';
+import Root from "./components/root";
 
 // We set this up in the last section
-import configureStore from './store/store';
+import configureStore from "./store/store";
 
 // We will use this to parse the user's session token
-import jwt_decode from 'jwt-decode';
+import jwt_decode from "jwt-decode";
 
 // The session utility we just created
-import { setAuthToken } from './util/session_api_util';
+import { setAuthToken } from "./util/session_api_util";
 
 // We have not created this action yet, but will do so in the next step
-import { logout } from './actions/session_actions';
+import { logout } from "./actions/session_actions";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   let store;
 
   // If a returning user has a session token stored in localStorage
   if (localStorage.jwtToken) {
-
     // Set the token as a common header for all axios requests
     setAuthToken(localStorage.jwtToken);
 
@@ -39,14 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
     if (decodedUser.exp < currentTime) {
       // Logout the user and redirect to the login page
       store.dispatch(logout());
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
   } else {
     // If this is a first time user, start with an empty store
     store = configureStore({});
   }
   // Render our root component and pass in the store as a prop
-  const root = document.getElementById('root');
+  const root = document.getElementById("root");
 
   ReactDOM.render(<Root store={store} />, root);
 });
