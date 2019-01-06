7699b27e34315877d478ee9ae59f78fd82a896af
kat <sarah.kathryn.peters@gmail.com>
2019-01-05 18:06:53 -0800

    Create basic structure for mainpage

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -8,12 +8,14 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <div className="game-player-score-box">
-          <div className="game-container"><Game /></div>
-          <div className="player-score-container"><PlayerScoreContainer /></div>
-        </div>
-        <div className="leaderboard-container">
-          <LeaderBoardContainer />
+        <div className="main-inner-container">
+          <div className="game-player-score-box">
+            <div className="game-container"><Game /></div>
+            <div className="player-score-container"><PlayerScoreContainer /></div>
+          </div>
+          <div className="leaderboard-container">
+            <LeaderBoardContainer />
+          </div>
         </div>
         <footer>
           Copyright &copy; 2019
