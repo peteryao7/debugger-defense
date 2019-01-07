40262b87e1f352b2942e3a83e8f4fd62100312c2
SHP <skylarprill@gmail.com>
2019-01-07 12:24:51 -0800

    Fix leaderboard overflow scroll

--- frontend/src/components/game/game_container.js
+++ frontend/src/components/game/game_container.js
@@ -4,19 +4,11 @@ import Game from './game';
 
 const mapStateToProps = state => {
   let currentUsername;
-<<<<<<< HEAD
-    if (state.session.user === undefined || Object.keys(state.session.user).length === 0) {
-      currentUsername = "LazyGuest";
-    } else {
-      currentUsername = state.session.user.username
-    }
-=======
   if (state.session.user === undefined || Object.keys(state.session.user).length === 0) {
     currentUsername = "LazyGuest";
   } else {
     currentUsername = state.session.user.username
   }
->>>>>>> 0c2042907d9cc3901deb46f98e7061fbe2fd16b3
   return {
     currentUsername
   };
