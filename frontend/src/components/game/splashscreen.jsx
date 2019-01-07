3233738c8e16839cddf387975417d8b8ac57de3b
SHP <skylarprill@gmail.com>
2019-01-07 11:18:34 -0800

    Write splashscreen text and style with colors

--- frontend/src/components/game/splashscreen.jsx
+++ frontend/src/components/game/splashscreen.jsx
@@ -8,6 +8,7 @@ const Splashscreen = (props) => {
         <p>Endless in number, the bugs will appear from the top and left side of your screen. 
           In order to stay alive, you must type out the bug's word before it reaches its destination: the _____ at the bottom-right!
           Beware, however! You have just adopted an endless fight, and the bugs don't get easier. </p>
+        <p>Think you're one of the greats? Sign in to make your mark on the leaderboard!</p>
       </div>
       <div className="start-button" onClick={() => props.startPlaying()}>
          <div className="start-button-text">Click to Start</div>
