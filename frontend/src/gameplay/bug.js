22f0b5b4035e1bf82b4de1e7eb42c0e8e0904bd8
kat <sarah.kathryn.peters@gmail.com>
2019-01-07 10:11:56 -0800

    Add basic sprite anim

--- frontend/src/gameplay/bug.js
+++ frontend/src/gameplay/bug.js
@@ -23,7 +23,8 @@ class Bug {
         this.yDiff = 600 - this.position[1];
         this.word = getRandomWord(difficulty);
         this.image = bugImage;
-
+        this.timeDrawn = (new Date()).getTime();
+        this.currentFrame = 0;
     }
 
     draw(ctx) {
@@ -31,14 +32,18 @@ class Bug {
         this.drawWord(ctx)
     }
 
-    updateFrame(ctx) {
-        ctx.drawImage(this.image, 160, 0, 160, 160, this.position[0], this.position[1], 90, 90)
+    drawBug(ctx) {
+        if (this.currentFrame % 20 === 0) {
+            ctx.drawImage(this.image, 0, 0, 160, 160, this.position[0], this.position[1], 90, 90);
+            this.currentFrame += 1;
+        } else {
+            ctx.drawImage(this.image, 160, 0, 160, 160, this.position[0], this.position[1], 90, 90)
+            this.currentFrame += 1;
+        }
     }
 
-    drawBug(ctx) {
-        ctx.drawImage(this.image, 0, 0, 160, 160, this.position[0], this.position[1], 90, 90);
-        // setInterval(() => this.updateFrame(ctx), 100)
-        //setinterval not working for anim currently
+    setUpdateInterval(ctx) {
+        setInterval(() => this.updateFrame(ctx), 100)
     }
 
     drawWord(ctx) {
