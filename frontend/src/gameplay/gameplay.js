8b4630f252404f35298e8a9fc279435c26318e3c
peteryao7@gmail.com <peteryao7@gmail.com>
2019-01-07 17:07:36 -0800

    Change difficulty back t o 1

--- frontend/src/gameplay/gameplay.js
+++ frontend/src/gameplay/gameplay.js
@@ -9,8 +9,8 @@ class GamePlay {
     this.currentUsername = currentUsername;
     this.ctx = ctx;
     this.destination = [970, 570];
-    this.difficulty = 10;
-    this.killCount = 200;
+    this.difficulty = 1;
+    this.killCount = 0;
     this.lives = 20;
     this.score = 0;
     this.secondsElapsed = 0;
