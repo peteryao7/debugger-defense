0e7a249dc2da0d9b8abb076f44b7ca2e761ad88d
SHP <skylarprill@gmail.com>
2019-01-08 15:32:51 -0800

    Add duck text to splashscreen and change line-height to improve readability

--- frontend/src/components/game/splashscreen.jsx
+++ frontend/src/components/game/splashscreen.jsx
@@ -5,9 +5,10 @@ const Splashscreen = (props) => {
     <div className="splash-screen">
       <div className="instructions">
         <h1>Time to squash some bugs!</h1>
-        <p>Endless in number, the bugs will appear from the top and left side of your screen. 
-          In order to stay alive, you must type out the bug's word before it reaches Production!
+        <p>Endless in number, the bugs skitter from the top and left side of your screen. 
+          In order to stay alive, type out the bug's word before it reaches Production!
           Beware, however! You have just adopted an endless fight, and the bugs don't get easier. </p>
+        <p>Psst... Need help? Talk to the ducks!</p>
         <p>Think you're one of the greats? Sign in to make your mark on the leaderboard!</p>
       </div>
       <div className="start-button" onClick={() => props.startPlaying()}>
